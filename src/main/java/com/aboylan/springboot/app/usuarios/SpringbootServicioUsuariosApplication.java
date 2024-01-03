6c4ae6656e64ded88ed285f25eb07ef40d93e83e
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-02 21:09:31 -0600

    Modificando servicio usuarios

--- src/main/java/com/aboylan/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ src/main/java/com/aboylan/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.aboylan.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.aboylan.springboot.app.commons.usuarios.models.entity"})
 @SpringBootApplication
 public class SpringbootServicioUsuariosApplication {
 
