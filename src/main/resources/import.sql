d29e0321eaff5e1da14d0b171c0b7d3773d9241c
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-04 12:37:53 -0600

    Probando la autenticacion con Postman y obteniendo el token JWT

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('alex', '12345', 1, 'Alex', 'Boylan', 'aboylan@correo.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'John', 'Doe', 'john.doe@correo.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('alex', '$2a$10$uZvxypO71.Ibe0Rct.Cbqe70i7HtlXJlCufKYaO3C/SxFE5KWAJqG', 1, 'Alex', 'Boylan', 'aboylan@correo.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$xWRgpLpBiG2Mj05KrLFJR.ckaar58EQqXqsEsPSgUVlVdRTtOefCq', 1, 'John', 'Doe', 'john.doe@correo.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
